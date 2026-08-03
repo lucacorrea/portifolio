f4e7f54fe17892262d3d8a85d4da85aed9ba21f3
Lucas De S. Correa <lucaasscorrea0@gmail.com>
2026-08-03 11:38:58 -0400

    Corrige acesso publico do SIGESP

--- sigesp/index.php
+++ sigesp/index.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+$requestPath = parse_url((string) ($_SERVER['REQUEST_URI'] ?? '/'), PHP_URL_PATH);
+if ($requestPath === '/sigesp/index.php') {
+    header('Location: /sigesp/', true, 302);
+    exit;
+}
+
 $frontController = __DIR__ . '/sigesporte/public/index.php';
 
 if (!is_file($frontController)) {
