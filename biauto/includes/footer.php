e117dc11b3678900d9d2df63fd451e7369209fc5
Lucas Correa <lucaasscorrea0@gmail.com>
2026-09-02 15:58:11 -0300

    chore(biauto): versiona javascript automaticamente

--- biauto/includes/footer.php
+++ biauto/includes/footer.php
@@ -1,8 +1,11 @@
-
+<?php
+$jsPath = dirname(__DIR__) . '/assets/js/app.js';
+$jsVersion = is_file($jsPath) ? (string) filemtime($jsPath) : (string) time();
+?>
     </main>
 </div>
 </div>
 <div class="sidebar-overlay" id="sidebarOverlay"></div>
-<script src="assets/js/app.js"></script>
+<script src="assets/js/app.js?v=<?= h($jsVersion) ?>"></script>
 </body>
 </html>
