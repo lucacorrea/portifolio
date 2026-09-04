08b96dd5c475c93bad5a8f3b34331839fbe096b0
Lucas Correa <lucaasscorrea0@gmail.com>
2026-09-04 03:44:49 -0300

    refactor(sigas): separa Governanca e Acessos em modulo publico

---
+++ sigas/governanca-acessos/matriz-acesso.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+$pageKey = 'matriz-acesso';
+require __DIR__ . '/_layout.php';
---
+++ sigas/governanca-acessos/setores.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+$pageKey = 'setores';
+require __DIR__ . '/_layout.php';
